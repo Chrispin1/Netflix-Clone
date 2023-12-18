c5f9812f8752c9d994bd0fe72e45b448584d223b
Chrispin <kiokondolo1@gmail.com>
2023-12-18 11:44:50 +0300

    added MovieRow component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import Hero from "../components/Hero";
 import Movierow from "../components/Movierow";
+import endpoints from "../services/movieServices";
 
 export const Home = () => {
   return (
     <>
       <Hero />
-      <Movierow />
-      <Movierow />
-      <Movierow />
+      <Movierow title="upcoming" url={endpoints.upcoming} />
+      <Movierow title="trending" url={endpoints.trending} />
+      <Movierow title="top rated" url={endpoints.topRated} />
+      <Movierow title="comedy" url={endpoints.comedy} />
+      <Movierow title="popular" url={endpoints.popular} />
     </>
   );
 };
